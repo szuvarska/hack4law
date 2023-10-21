d5d190d9d7032bf98b2be3361b18a010a429a716
Marta Szuwarska <marta.szuwarska.stud@pw.edu.pl>
2023-10-21 14:21:15 +0200

    Update preprocessing.py

--- Preprocessing/preprocessing.py
+++ Preprocessing/preprocessing.py
@@ -3,12 +3,14 @@ import textwrap
 import pandas as pd
 
 # Replace 'your_file.csv' with the path to your CSV file
-file_path = '../Data/Out_177.csv'
+file_path = '../Data/output3.csv'
 
 # Read the CSV file into a DataFrame
-df = pd.read_csv(file_path)
+df = pd.read_csv(file_path, sep=";")
 df = df.drop(df.columns[0], axis=1)
-# print(df.info())
+print(df.info())
 
 # Print the pretty printed string
-print(textwrap.fill(df["textContent"][0], width=60))
+# print(textwrap.fill(df["textContent"][0], width=60))
+
+print(df["courtType"].unique())
