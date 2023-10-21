0fe3af358e22313f321130eb47a99913f4a208f4
BorkowskiMaciej <maciej.borkowski@op.pl>
2023-10-21 23:59:39 +0200

    changes in frontend

--- Front/data_process.py
+++ Front/data_process.py
@@ -1,16 +1,11 @@
 import pandas as pd
-import json
 import re
-df = pd.read_csv("clean_output1.csv", sep=';')
+df = pd.read_csv("clean_output2.csv", sep=';')
 pd.options.mode.chained_assignment = None
-# print(df)
-
-# print(df['judges'])
 
 def get_judges(df):
     pattern = r"'name': '(.*?)',"
     for i in range (df.shape[0]):
-        
         df['judges'][i] = re.findall(pattern, df['judges'][i])
 
     return df
@@ -21,12 +16,5 @@ def get_caseNumber(df):
         df['courtCases'][i] = re.findall(pattern, df['courtCases'][i])
     return df
 
-
-
-
 df_r = get_judges(df)
 df_r = get_caseNumber(df_r)
-
-# imiona = [osoba['name'] for osoba in df['judges']]
-
-
