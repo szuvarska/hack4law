3fdbd2e850b887247abb0197dd04ca333ac5212d
Łukasz Lepianka <Lukis30888@gmail.com>
2023-10-21 15:05:20 +0200

    add definition of addSimilarity function

--- Similarity/similarity.py
+++ Similarity/similarity.py
@@ -1,4 +1,19 @@
 import pandas as pd
 
-df = pd.read_csv('Data/output3.csv', sep = ";")
-print(df.loc[df["courtType"]=='COMMON',])
+df = pd.read_csv('Data/output4.csv', sep = ";")
+df = df.loc[df["courtType"]=='COMMON',]
+print(df['textContent'])
+
+
+def addSimilarity(new_data:pd.Series, base_df:pd.DataFrame):
+    """
+    This functions will have documentation later
+
+    Args:
+        base_df (pd.DataFrame): _description_
+        new_data (pd.Series): _description_
+
+    Returns:
+        _type_: _description_
+    """    
+    pass
