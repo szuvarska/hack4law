bee21e4be030cc1fc843762ef384c00a16e15b27
Marta Szuwarska <marta.szuwarska.stud@pw.edu.pl>
2023-10-22 02:52:14 +0200

    Update output for frontend

--- Data_uploader/making_data_for_frontend.py
+++ Data_uploader/making_data_for_frontend.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-file_path1 = '../Data/with_uzasadnienie2.csv'
+file_path1 = '../Data/with_uzasadnienie2_tags.csv'
 df1 = pd.read_csv(file_path1, sep=";")
-df1= df1[["id","uzasadnienie", "judges","courtCases","judgmentDate"]]
+df1= df1[["id","uzasadnienie", "judges","courtCases","judgmentDate", "tags"]]
 file_path2 = '../Data/output_with_decisions2.csv'
 df2 = pd.read_csv(file_path2, sep=";")
-df2 = df2[["id","textContent"]]
+df2 = df2[["id","textContent", "extractedDecision"]]
 df = pd.merge(df1, df2, on='id', how='inner')
-df.drop(df.index[-850:], inplace=True)
+df.drop(df.index[-900:], inplace=True)
 
-df.to_csv("../Data/output_for_frontend.csv", sep=";", index=False)
+df.to_csv("../Data/output_for_frontend_2.csv", sep=";", index=False)
